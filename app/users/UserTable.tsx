ca420666698dbf82b5c0b107cbce9f56fac9882a
SagirGarba <sagir40me@gmail.com>
2024-11-26 16:25:03 +0100

    Visualize the user table

--- app/users/UserTable.tsx
+++ app/users/UserTable.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { sort } from "fast-sort";
 
 interface Users {
+  address: { city: string }; 
   id: number;
   name: string;
   email: string;
@@ -10,6 +11,7 @@ interface Users {
 
 interface Props {
   sortOrder: string;
+  sortOrder1?: string; 
 }
 
 const UserTable = async ({ sortOrder }: Props) => {
@@ -18,28 +20,40 @@ const UserTable = async ({ sortOrder }: Props) => {
   });
   const users: Users[] = await res.json();
 
-  const sortedUsers = sort(users).asc(
-    sortOrder === "email" ? (user) => user.email : (user) => user.name
-  );
+  const sortedUsers = sort(users).asc((user) => {
+    if (sortOrder === "email") return user.email;
+    if (sortOrder === "name") return user.name;
+    if (sortOrder === "city") return user.address.city;
+    if (sortOrder === "id") return user.id;
+    return user.id; 
+  });
+
   return (
     <>
       <table className="table table-bordered">
         <thead>
           <tr>
             <th>
-              <Link href="users?sortOrder=name">Name</Link>
+              <Link href="/users?sortOrder=id">Id</Link>
             </th>
             <th>
-              <Link href="users?sortOrder=email">Email</Link>
+              <Link href="/users?sortOrder=name">Name</Link>
+            </th>
+            <th>
+              <Link href="/users?sortOrder=email">Email</Link>
+            </th>
+            <th>
+              <Link href="/users?sortOrder=city">City</Link>
             </th>
           </tr>
         </thead>
-
         <tbody>
           {sortedUsers.map((user) => (
             <tr key={user.id}>
+              <td>{user.id}</td>
               <td>{user.name}</td>
               <td>{user.email}</td>
+              <td>{user.address.city}</td>
             </tr>
           ))}
         </tbody>
--- middleware.ts
+++ middleware.ts
@@ -4,5 +4,5 @@ export function middleware(request: NextRequest) {
   return NextResponse.redirect(new URL("/new-page", request.url));
 }
 export const config = {
-  matcher: ["/users"],
+  matcher: ["/user"],
 };
